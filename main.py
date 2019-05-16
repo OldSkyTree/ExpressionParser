9621f5b381b934eb71b611eb5ce012c931ac7039
Ilyas Hassen <IlyasHassen@gmail.com>
2019-05-16 15:36:05 +0300

    Complete both parse and execute functions

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ operators = {"+": operator.add, "-": operator.sub, "*": operator.mul, "/": opera
 operators_priority = {"+": 0, "-": 0, "*": 1, "/": 1, "^": 2}
 
 
-def check_expression(expression):
-    pass
+def execute(reverse_polish):
+    stack = []
+    for lexeme in reverse_polish:
+        if is_number(lexeme):
+            stack.append(lexeme)
+        else:
+            stack.append(operators[lexeme](float(stack.pop(-2)), float(stack.pop())))
+    return stack.pop()
 
 
 def convert_to_reverse_polish_notation(expression):
@@ -43,10 +49,22 @@ def get_lexemes(expression):
     lexemes = []
     i = 0
     while i < len(expression):
-        if (i + 1) < len(expression) and expression[i + 1] == '.':
+        if len(expression) > (i + 1) and expression[i].isdigit() and expression[i + 1] == '.':
             j = i + 2
             while j < len(expression):
                 if not expression[j].isdigit():
+                    lexemes.append(expression[i:j])
+                    i = j - 1
+                    break
+                j += 1
+        elif len(expression) > (i + 1) and expression[i].isdigit() and expression[i + 1].isdigit():
+            j = i + 1
+            while j < len(expression):
+                if j == len(expression) - 1:
+                    lexemes.append(expression[i:j + 1])
+                    i = j
+                    break
+                elif not expression[j].isdigit():
                     lexemes.append(expression[i:j])
                     i = j - 1
                     break
@@ -56,6 +74,11 @@ def get_lexemes(expression):
         i += 1
     return lexemes
 
+
+exp = input("Введите выражение: ")
+rev_polish = convert_to_reverse_polish_notation(exp)
+result = execute(rev_polish)
+print("Ответ", result)
 
 # (8+2*5.5)/(1+3*2-4^1)
 # 825.5*+132*+41^-/
